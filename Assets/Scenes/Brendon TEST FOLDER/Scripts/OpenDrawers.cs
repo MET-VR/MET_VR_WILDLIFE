0ec30c6d558ba1597c70b5bfc81abc3e9aa9ee1d
Brendon Gacusan <bbgacusan1@gmail.com>
2025-01-07 09:59:45 -0700

    Modifying the Cabinet
    New way to open - only moves when we grab the handle rather than whenever you grab anywhere near the box. We also want to see if we should only have one cabinet drawer open. I also removed the quit button from the starting zone - not functional in the first place.

---
+++ Assets/Scenes/Brendon TEST FOLDER/Scripts/OpenDrawers.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDrawers : MonoBehaviour
+{
+    private string isD1open = "n";
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
