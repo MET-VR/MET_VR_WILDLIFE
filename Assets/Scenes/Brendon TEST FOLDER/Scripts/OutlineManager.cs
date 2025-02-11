1d2e5a9fedc450aaaba852d73ece104d04854d53
brenster24 <bbgacusan1@gmail.com>
2025-02-11 08:31:40 -0700

    Added new room and tried getting outlines to work

---
+++ Assets/Scenes/Brendon TEST FOLDER/Scripts/OutlineManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineManager : MonoBehaviour
+{
+    public Outline[] outline;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //outline[0].
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
